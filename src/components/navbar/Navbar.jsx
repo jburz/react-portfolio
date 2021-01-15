f8a47b722ef8dffdf102610b25cd7944eb6d6275
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-15 11:50:30 -0700

    added navbar links to homepage, portfolio and contact page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 
 const MyNavbar = () => {
     return (
-        <Navbar>
-            <h3>Navbar</h3>
-
-        </Navbar>
+        <>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <LinkContainer to="/profile">
+                    <Navbar.Brand className="sure-vote">SURE VOTE</Navbar.Brand>
+                </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/portfolio">
+                            <Nav.Link>Portfolio</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/contact">
+                            <Nav.Link>Contact</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
     )
 }
 
