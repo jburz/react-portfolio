d1972c7934e3b5c1218a979bb97c37df296ddc82
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-15 11:53:58 -0700

    added styling to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import "./Navbar.css";
 
 
 const MyNavbar = () => {
