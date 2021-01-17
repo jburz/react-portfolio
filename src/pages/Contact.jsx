c1cf567d2bd96a811ce2bbeb76ee972df8d1487a
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-16 20:59:52 -0700

    added social media contact info to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import MyNavbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -6,7 +7,34 @@ const Contact = () => {
     return (
         <>
             <MyNavbar />
-            <h1>Contact</h1>
+            <Container>
+                <Row className="mt-5 text-light">
+                    <h1>Contact Info</h1>
+                </Row>
+                <Row className="text-light">
+                    <p>
+                        Find me on social media!
+                    </p>
+                </Row>
+                <Row>
+                    <Col sm={2} className="d-flex justify-content-center">
+                        <i className="fab fa-linkedin icon"></i>
+                        <a href="https://www.linkedin.com/in/jake-burzlaff-4b03a0b5/" target="_blank" rel="noreferrer" className="text">LinkedIn</a>
+                    </Col>
+                    <Col sm={2} className="d-flex justify-content-center">
+                        <i className="fab fa-github icon"></i>
+                        <a href="https://www.linkedin.com/in/jake-burzlaff-4b03a0b5/" target="_blank" rel="noreferrer" className="text">GitHub</a>
+                    </Col>
+                    <Col sm={3} className="d-flex justify-content-center">
+                        <i className="fas fa-phone icon"></i>
+                        <a href="https://www.linkedin.com/in/jake-burzlaff-4b03a0b5/" target="_blank" rel="noreferrer" className="text">(630) 809-8335</a>
+                    </Col>
+                    <Col sm={3} className="d-flex justify-content-center">
+                        <i className="fas fa-envelope icon"></i>
+                        <a href="https://www.linkedin.com/in/jake-burzlaff-4b03a0b5/" target="_blank" rel="noreferrer" className="text">jake.bilbao.04.06@gmail.com</a>
+                    </Col>
+                </Row>
+            </Container>
             <Footer />
         </>
 
