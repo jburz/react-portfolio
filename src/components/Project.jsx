520eb6e9704da2d5203d3f1e75e4730cbe21d80a
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-16 19:17:56 -0700

    built out project component with card template to use for each project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import "../assets/css/style.css";
 
 const Project = () => {
     return (
-        <p>Hit project again</p>
+
+        <Card className="card-border">
+            <Card.Img src="" alt="placeholder for project" className="projectImg img-fluid img-thumbnail img-border" />
+            <Card.Body>
+                <Card.Title>Placeholder Tiltle</Card.Title>
+                <Card.Text>Placeholder Text</Card.Text>
+                <a className="btn btn-sm btn-style" target="_blank">Github</a>
+                <a className="btn btn-sm btn-style" target="_blank">Deployed Site</a>
+            </Card.Body>
+        </Card>
 
     )
 }
