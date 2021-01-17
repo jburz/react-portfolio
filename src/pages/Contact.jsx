b134e3b8643191a6532d9865183789a492aedd6b
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-16 18:30:52 -0700

    add contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MyNavbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Contact = () => {
+    return (
+        <>
+            <MyNavbar />
+            <h1>Contact</h1>
+            <Footer />
+        </>
+
+    )
+}
+
+export default Contact;
