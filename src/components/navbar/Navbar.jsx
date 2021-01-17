b967ac3b6708bc9989574ec7351164446f994c19
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2021-01-16 17:53:09 -0700

    added headshot image, styled border, added about me and button to reference resume

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import "./Navbar.css";
+import "../../assets/css/Navbar.css";
 
 
 const MyNavbar = () => {
